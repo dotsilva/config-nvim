b282dc83b849d5041fd512de3934ab26eb5938d3
Bruno da Silva <dotsilva@proton.me>
2026-05-17 23:53:04 -0300

    changing xterm to xterm9

--- lua/config/xterm9.lua
+++ lua/config/xterm9.lua
@@ -1,6 +1,6 @@
 vim.cmd 'hi clear'
 if vim.fn.exists 'syntax_on' then vim.cmd 'syntax reset' end
-vim.g.colors_name = 'xterm'
+vim.g.colors_name = 'xterm9'
 vim.o.background = 'dark'
 
 -- color palette
