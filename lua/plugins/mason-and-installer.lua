02a831f121a56206dab3d4bcf05e07366ba08cf3
Bruno da Silva <dotsilva@proton.me>
2026-05-24 14:53:31 -0300

    optimizing ensure installed list, removing not used tools

--- lua/plugins/mason-and-installer.lua
+++ lua/plugins/mason-and-installer.lua
@@ -1,27 +1,30 @@
 return {
-{
-'mason-org/mason.nvim',
-opts = { ui = { border = 'double' } },
-},
-{
-  'WhoIsSethDaniel/mason-tool-installer.nvim',
-  config = function()
-    local ensure_installed = {
-      'clangd', -- c
-      'biome', -- css, html, json
-      'luacheck', -- lua
-      'mdformat', -- markdown
-      'ols', -- odin
-      'qmlls', -- qml
-      'shfmt', -- bash
-      'shellcheck', -- bash
-      'sqlfluff', -- sql
-      'stylua', -- lua
-      'taplo', -- toml, ini
-      'tree-sitter-cli', -- required for nvim-treesitter
-    }
+  {
+    'mason-org/mason.nvim',
+    opts = { ui = { border = 'double' } },
+  },
+  {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    config = function()
+      local ensure_installed = {
+        'clang-format', -- c
+        'cpplint', -- c
+        'biome', -- css, javascript, json, jsonc
+        'htmlhint', -- html
+        'htmlbeautifier', -- html
+        'luacheck', -- lua
+        'mdformat', -- markdown
+        'ols', -- odin
+        'qmlls', -- qml
+        'shfmt', -- bash
+        'shellcheck', -- bash
+        'sql_formatter', -- sql
+        'stylua', -- lua
+        'taplo', -- toml, ini
+        'tree-sitter-cli', -- required by nvim-treesitter
+      }
 
-    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-  end,
-},
+      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+    end,
+  },
 }
