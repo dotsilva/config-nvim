225c4c101f8c617dc1d0978ed1d2689df6f1b061
Bruno da Silva <dotsilva@proton.me>
2026-05-26 13:28:33 -0300

    formatter config finished

--- lua/plugins/conform.nvim.lua
+++ lua/plugins/conform.nvim.lua
@@ -18,30 +18,42 @@ return {
     opts = {
         notify_on_error = false,
         format_on_save = {
-            timeout_ms = 500,
+            timeout_ms = 1000,
             lsp_format = 'fallback',
         },
         formatters_by_ft = {
             c = { 'clang-format' },
             css = { 'biome' },
             dosini = { 'taplo' },
-            html = { 'htmlbeautifier' }, -- alt biome, but i couldnt make it work
+            html = { 'htmlbeautifier' }, --alt biome, but i couldnt make it work
             javascript = { 'biome' },
             json = { 'biome' },
             jsonc = { 'biome' },
             lua = { 'stylua' },
-            markdown = { 'mdformat' },
-            -- odin = { 'odinfmt' },
+            markdown = { 'mdformat', 'injected' }, --injected handle code blocks to their formatters
+            --odin = { 'odinfmt' },
             qml = { 'qmlformat' },
             sh = { 'shfmt' },
-            sql = { 'sql_formatter' },
+            --sql = { 'sql_formatter' }, --alternative formatter for sqlite
+            sql = { 'sqlfluff' },
             toml = { 'taplo' },
             zig = { 'zigfmt' },
         },
         formatters = {
-            sql_formatter = {
-                args = { '-l', 'sqlite' },
+            sqlfluff = {
+                cwd = false,
+                args = {
+                    'format',
+                    '--templater',
+                    'raw',
+                    '--dialect',
+                    'sqlite',
+                    '-',
+                },
             },
+            --sql_formatter = {
+            --    args = { '-l', 'sqlite' },
+            --},
         },
     },
 }
