37db9eeb6a2f827ef79af1394cdbbe89dfda6be2
Bruno da Silva <dotsilva@proton.me>
2026-05-28 14:27:47 -0300

    simplifying pick config

--- lua/plugins/mini.pick.lua
+++ lua/plugins/mini.pick.lua
@@ -3,28 +3,23 @@ return {
     dependencies = { 'nvim-mini/mini.extra' },
     keys = {
         {
-            '<leader><leader>',
-            function() require('mini.pick').builtin.buffers() end,
-            desc = 'Find existing buffers',
-        },
-        {
             '<leader>sf',
             function() require('mini.pick').builtin.files() end,
             desc = 'Search Files',
         },
         {
-            '<leader>sg',
+            '<leader>sw',
             function() require('mini.pick').builtin.grep_live() end,
-            desc = 'Search by Grep',
+            desc = 'Search Words',
         },
         {
-            '<leader>sw',
+            '<leader>sc',
             function()
                 require('mini.pick').builtin.grep {
                     pattern = vim.fn.expand '<cword>',
                 }
             end,
-            desc = 'Search current Word',
+            desc = 'Search Current Word',
         },
         {
             '<leader>sh',
@@ -33,28 +28,8 @@ return {
         },
         {
             '<leader>sr',
-            function() require('mini.pick').builtin.resume() end,
-            desc = 'Search Resume',
-        },
-        {
-            '<leader>sd',
-            function() require('mini.extra').pickers.diagnostic() end,
-            desc = 'Search Diagnostics',
-        },
-        {
-            '<leader>s.',
             function() require('mini.extra').pickers.oldfiles() end,
             desc = 'Search Recent Files',
-        },
-        {
-            '<leader>sk',
-            function() require('mini.extra').pickers.keymaps() end,
-            desc = 'Search Keymaps',
-        },
-        {
-            '<leader>sc',
-            function() require('mini.extra').pickers.commands() end,
-            desc = 'Search Commands',
         },
         {
             '<leader>sn',
@@ -66,15 +41,8 @@ return {
             desc = 'Search Neovim files',
         },
     },
-    opts = {
-        window = {
-            config = { border = 'double' },
-        },
-    },
     config = function(_, opts)
         require('mini.pick').setup(opts)
         require('mini.extra').setup()
-        -- Override default UI select to use mini.pick
-        vim.ui.select = require('mini.pick').ui_select
     end,
 }
