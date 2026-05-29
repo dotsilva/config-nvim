fc1d1242d79dfd32b1e2804da04dd0683dfa8e8e
Bruno da Silva <dotsilva@proton.me>
2026-05-29 14:18:10 -0300

    adding custom recipe for superhtml as linter

--- lua/plugins/lint_f.lua
+++ lua/plugins/lint_f.lua
@@ -1,11 +1,12 @@
 do
     vim.pack.add { 'https://github.com/mfussenegger/nvim-lint' }
+
     local lint = require 'lint'
     lint.linters_by_ft = {
         --commented tools need configuration
         c = { 'clangtidy' }, --system: clang, which is called as clang-tidy in cli
         css = { 'biomejs' }, --mason: biome
-        html = { 'htmlhint' }, --alt biome, but it is not plug and play neither i could make it work
+        html = { 'superhtml' }, --mason --alt biome, but i couldnt make it work
         javascript = { 'biomejs' }, --mason: biome
         json = { 'biomejs' }, --mason: biome
         jsonc = { 'biomejs' }, --mason: biome
@@ -13,6 +14,29 @@ do
         sh = { 'shellcheck' }, --mason
         sql = { 'sqlfluff' }, --mason
         zig = { 'zig' }, --system
+    }
+
+    local superhtml_pattern = '^([^:]+):(%d+):(%d+): (.+)$'
+    local superhtml_groups = { 'file', 'lnum', 'col', 'message' }
+    local superhtml_severity_map = {
+        ['error'] = vim.diagnostic.severity.ERROR,
+    }
+    lint.linters.superhtml = {
+        cmd = 'superhtml',
+        stdin = false,
+        args = {
+            'check' --[['--syntax-only']],
+        },
+        stream = 'stderr',
+        ignore_exitcode = true,
+        parser = require('lint.parser').from_pattern(
+            superhtml_pattern,
+            superhtml_groups,
+            superhtml_severity_map,
+            {
+                source = 'superhtml',
+            }
+        ),
     }
 
     lint.linters.clangtidy.args = {
