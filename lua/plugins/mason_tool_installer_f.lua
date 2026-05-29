441229cf92e5fffcfef0b05dcbafe3c5352c146d
Bruno da Silva <dotsilva@proton.me>
2026-05-29 14:49:40 -0300

    handling termux not suported mason pkgs

--- lua/plugins/mason_tool_installer_f.lua
+++ lua/plugins/mason_tool_installer_f.lua
@@ -2,19 +2,37 @@ do
     vim.pack.add {
         'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim',
     }
-    local ensure_installed = {
-        'clang-format', --c
-        'biome', --css, javascript, json, jsonc
-        'luacheck', --lua
-        'mdformat', --markdown
-        'shfmt', --bash
-        'shellcheck', --bash
-        'sqlfluff', --sql
-        'stylua', --lua
-        'superhtml', --html
-        'taplo', --toml, ini
-        'tree-sitter-cli', --required by nvim-treesitter
-    }
+    local ensure_installed
+    local is_termux = vim.loop.fs_stat '/data/data/com.termux'
+    if is_termux then
+        ensure_installed = {
+            'clang-format', --c
+            'biome', -- css, javascript, json, jsonc
+            -- 'luacheck', -- lua
+            'mdformat', -- markdown
+            'shfmt', -- bash
+            'shellcheck', -- bash
+            'sqlfluff', -- sql
+            -- 'stylua', -- lua
+            'superhtml', -- html
+            'taplo', -- toml, ini
+            'tree-sitter-cli', -- required by nvim-treesitter
+        }
+    else
+        ensure_installed = {
+            'clang-format', -- c
+            'biome', -- css, javascript, json, jsonc
+            'luacheck', -- lua
+            'mdformat', -- markdown
+            'shfmt', -- bash
+            'shellcheck', -- bash
+            'sqlfluff', -- sql
+            'stylua', -- lua
+            'superhtml', -- html
+            'taplo', -- toml, ini
+            'tree-sitter-cli', -- required by nvim-treesitter
+        }
+    end
 
     require('mason-tool-installer').setup {
         ensure_installed = ensure_installed,
