760badfa78826e6cceced4c315fece29c6c117a6
Bruno da Silva <dotsilva@proton.me>
2026-06-05 19:39:35 -0300

    add ff, fg, fr and fb for fuzzy find, grep, recent and buffers

--- lua/plugins/fzf_lua_f.lua
+++ lua/plugins/fzf_lua_f.lua
@@ -1,6 +1,31 @@
 vim.pack.add { 'https://github.com/ibhagwan/fzf-lua' }
 require('fzf-lua').setup {}
 
+vim.keymap.set(
+    'n',
+    '<leader>ff',
+    function() require('fzf-lua').files() end,
+    { desc = 'find' }
+)
+vim.keymap.set(
+    'n',
+    '<leader>fg',
+    function() require('fzf-lua').live_grep() end,
+    { desc = 'grep' }
+)
+vim.keymap.set(
+    'n',
+    '<leader>fr',
+    function() require('fzf-lua').oldfiles() end,
+    { desc = 'recent' }
+)
+vim.keymap.set(
+    'n',
+    '<leader>fb',
+    function() require('fzf-lua').buffers() end,
+    { desc = 'buffers' }
+)
+
 -- TODO: set the keymaps for fzf-lua
 -- TODO: read this: https://echasnovski.com/blog/2026-03-13-a-guide-to-vim-pack
 -- TODO: try this convention instead
