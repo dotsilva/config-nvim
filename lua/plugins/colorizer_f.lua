68662f8fc22b47fd19d5aabc5ce43d234fb9089d
Bruno da Silva <dotsilva@proton.me>
2026-06-08 00:26:11 -0300

    fix bad formatting

--- lua/plugins/colorizer_f.lua
+++ lua/plugins/colorizer_f.lua
@@ -5,15 +5,12 @@ require('colorizer').setup {
 			default = true, -- enable all hex options
 			rgb = false, -- #RGB (3-digit) -- turned off
 			rgba = false, -- #RGBA (4-digit) -- turned off
-
-			--[[
-                --the config above already enables these ones
-                --but i am leting them here for documenting it
-                rrggbb = true, -- #RRGGBB (6-digit)
-                rrggbbaa = true, -- #RRGGBBAA (8-digit)
-                hash_aarrggbb = true, -- #AARRGGBB (QML-style, alpha first)
-                no_hash = true, -- hex without '#' at word boundaries
-                ]]
+			-- the config above already enables these ones
+			-- but i am leting them here for documenting it
+			-- rrggbb = true, -- #RRGGBB (6-digit)
+			-- rrggbbaa = true, -- #RRGGBBAA (8-digit)
+			-- hash_aarrggbb = true, -- #AARRGGBB (QML-style, alpha first)
+			-- no_hash = true, -- hex without '#' at word boundaries
 		},
 		rgb = { enable = true },
 		xterm = { enable = true },
